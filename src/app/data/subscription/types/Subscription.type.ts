3c7e9027a5ece2133e36d877a8396f5675276cea
Prabir Vora <pvora30@gatech.edu>
2021-07-27 18:05:02 -0700

    Add color code automatically

--- src/app/data/subscription/types/Subscription.type.ts
+++ src/app/data/subscription/types/Subscription.type.ts
@@ -53,8 +53,21 @@ export class SubscriptionObject {
       const channel = c.toObject()
       if (channel.colorCode) {
         return channel.colorCode
+      } else {
+        // If no color code then randomly assign a color code from our list
+        const colorChoices = ["#f44336", "#e91e63", "#9c27b0", "#673ab7", "#3f51b5", "#2196f3", "#03a9f4", "#00bcd4", "#009688", "#4caf50", "#8bc34a", "#cddc39", "#ff5722", "#795548"]
+
+        const randomColor = colorChoices[Math.floor(Math.random() * colorChoices.length)];
+
+        await ChannelModel.updateOne({
+          _id: channelId
+        }, {
+          colorCode: randomColor
+        })
+        
+        return randomColor;
       }
-      return ""
+      
     }
     return ''
   }
